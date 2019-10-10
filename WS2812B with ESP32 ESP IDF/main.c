827db79c3694f8a2529349b6ac3f9e0ca6154cc0
ENGRsao <akeemolalekan229@gmail.com>
2019-10-10 13:55:11 +0100

    added rainbow algorithm

--- WS2812B with ESP32 ESP IDF/main.c
+++ WS2812B with ESP32 ESP IDF/main.c
@@ -16,11 +16,77 @@
 #define INT_PIN 15
 int blink_status = 0;
 
+
+// Configure these based on your project needs ********
+#define LED_RMT_TX_CHANNEL RMT_CHANNEL_0
+#define LED_RMT_TX_GPIO 18
+// ****************************************************
+
+#define BITS_PER_LED_CMD 24
+#define LED_BUFFER_ITEMS ((NUM_LEDS * BITS_PER_LED_CMD))
+
+// These values are determined by measuring pulse timing with logic analyzer and adjusting to match datasheet.
+#define T0H 14  // 0 bit high time
+#define T1H 36 //52  // 1 bit high time
+#define T0L 36 //52  // low time for either bit
+#define T1L 14
+
+#define RED 0x006400
+#define ORANGE 0x426400
+#define YELLOW 0x646400
+#define GREEN  0x640000
+#define BLUE 0x000064
+#define INDIGO 0x003264
+#define VIOLET 0x306464
+#define OFF 0x000000
+
+uint32_t rainbow_sequence[11] = {OFF,OFF,RED,ORANGE,YELLOW,GREEN,BLUE,INDIGO,VIOLET,OFF,OFF};
+
+#ifndef NUM_LEDS
+#define NUM_LEDS 3
+#endif
+struct led_state new_state;
+// This structure is used for indicating what the colors of each LED should be set to.
+// There is a 32bit value for each LED. Only the lower 3 bytes are used and they hold the
+// Red (byte 2), Green (byte 1), and Blue (byte 0) values to be set.
+struct led_state {
+    uint32_t leds[NUM_LEDS];
+};
+// This is the buffer which the hw peripheral will access while pulsing the output pin
+rmt_item32_t led_data_buffer[LED_BUFFER_ITEMS];
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
 	blink_status = !blink_status;
 }
 
+
+void setup_rmt_data_buffer(struct led_state new_state)
+{
+  for (uint32_t led = 0; led < NUM_LEDS; led++) {
+    uint32_t bits_to_send = new_state.leds[led];
+    uint32_t mask = 1 << (BITS_PER_LED_CMD - 1);
+    for (uint32_t bit = 0; bit < BITS_PER_LED_CMD; bit++) {
+      uint32_t bit_is_set = bits_to_send & mask;
+      led_data_buffer[led * BITS_PER_LED_CMD + bit] = bit_is_set ?
+                                                      (rmt_item32_t){{{T1H, 1, T1L, 0}}} :
+                                                      (rmt_item32_t){{{T0H, 1, T0L, 0}}};
+      mask >>= 1;
+    }
+  }
+}
+void ws2812_write_leds(struct led_state new_state) {
+  setup_rmt_data_buffer(new_state);
+  ESP_ERROR_CHECK(rmt_write_items(LED_RMT_TX_CHANNEL, led_data_buffer, LED_BUFFER_ITEMS, false));
+  ESP_ERROR_CHECK(rmt_wait_tx_done(LED_RMT_TX_CHANNEL, portMAX_DELAY));
+}
+void rainbowRunUp(uint8_t val)
+{
+		  new_state.leds[0] = rainbow_sequence[val];
+		  new_state.leds[1] = rainbow_sequence[val+1];
+		  new_state.leds[2] = rainbow_sequence[val+2];
+		  ws2812_write_leds(new_state);
+
+}
 
 void hello_task(void *pvParameter)
 {
@@ -31,13 +97,15 @@ void hello_task(void *pvParameter)
     gpio_set_intr_type(INT_PIN,GPIO_INTR_POSEDGE);
     gpio_isr_handler_add(INT_PIN, gpio_isr_handler, (void*) INT_PIN);
 	gpio_intr_enable(INT_PIN);
+	int i = 0;
 	while(1)
 	{
-		if (blink_status == 1)
-		{
 			printf("Hello world!\n");
-	        vTaskDelay(100 / portTICK_RATE_MS);
-		}
+
+			rainbowRunUp(i++);
+			if (i > 9) i = 0;
+	        vTaskDelay(1000 / portTICK_RATE_MS);
+
 	}
 }
 
@@ -58,43 +126,10 @@ void blinky(void *pvParameter)
     		while(gpio_get_level(BUTTON_PIN) == 0);
     	}
         gpio_set_level(BLINK_GPIO, level);
-       // vTaskDelay(1000 / portTICK_RATE_MS);
-        /* Blink on (output high) */
-        //gpio_set_level(BLINK_GPIO, 1);
-        //vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
 
 
-// Configure these based on your project needs ********
-#define LED_RMT_TX_CHANNEL RMT_CHANNEL_0
-#define LED_RMT_TX_GPIO 18
-// ****************************************************
-
-#define BITS_PER_LED_CMD 24
-#define LED_BUFFER_ITEMS ((NUM_LEDS * BITS_PER_LED_CMD))
-
-// These values are determined by measuring pulse timing with logic analyzer and adjusting to match datasheet.
-#define T0H 14  // 0 bit high time
-#define T1H 36 //52  // 1 bit high time
-#define T0L 36 //52  // low time for either bit
-#define T1L 14
-#define RED   0x000000
-#define GREEN 0x0000FF
-#define BLUE  0x000000
-
-#ifndef NUM_LEDS
-#define NUM_LEDS 3
-#endif
-
-// This structure is used for indicating what the colors of each LED should be set to.
-// There is a 32bit value for each LED. Only the lower 3 bytes are used and they hold the
-// Red (byte 2), Green (byte 1), and Blue (byte 0) values to be set.
-struct led_state {
-    uint32_t leds[NUM_LEDS];
-};
-// This is the buffer which the hw peripheral will access while pulsing the output pin
-rmt_item32_t led_data_buffer[LED_BUFFER_ITEMS];
 
 void setup_rmt_data_buffer(struct led_state new_state);
 
@@ -115,39 +150,16 @@ void ws2812_control_init(void)
   ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
 }
 
-void ws2812_write_leds(struct led_state new_state) {
-  setup_rmt_data_buffer(new_state);
-  ESP_ERROR_CHECK(rmt_write_items(LED_RMT_TX_CHANNEL, led_data_buffer, LED_BUFFER_ITEMS, false));
-  ESP_ERROR_CHECK(rmt_wait_tx_done(LED_RMT_TX_CHANNEL, portMAX_DELAY));
-}
 
-void setup_rmt_data_buffer(struct led_state new_state)
-{
-  for (uint32_t led = 0; led < NUM_LEDS; led++) {
-    uint32_t bits_to_send = new_state.leds[led];
-    uint32_t mask = 1 << (BITS_PER_LED_CMD - 1);
-    for (uint32_t bit = 0; bit < BITS_PER_LED_CMD; bit++) {
-      uint32_t bit_is_set = bits_to_send & mask;
-      led_data_buffer[led * BITS_PER_LED_CMD + bit] = bit_is_set ?
-                                                      (rmt_item32_t){{{T1H, 1, T1L, 0}}} :
-                                                      (rmt_item32_t){{{T0H, 1, T0L, 0}}};
-      mask >>= 1;
-    }
-  }
-}
+
+
 
 void app_main()
 {
 	 ws2812_control_init();
 
-	  struct led_state new_state;
-	  new_state.leds[0] = RED;
-	  new_state.leds[1] = GREEN;
-	  new_state.leds[2] = BLUE;
-	  ws2812_write_leds(new_state);
-    //nvs_flash_init();
+	  nvs_flash_init();
 
-    //xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
-    //xTaskCreate(&blinky, "blinky", 512,NULL,5,NULL );
-    //vTaskStartScheduler();
+	  xTaskCreate(hello_task, "hello_task", 2048, NULL, 5, NULL);
+	  xTaskCreate(blinky, "blinky", 512,NULL,5,NULL );
 }
