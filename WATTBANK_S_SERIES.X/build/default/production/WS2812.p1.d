bbbe3ab55c16d2528348b79392d010dce6d2fbb8
ENGRsao <akeemolalekan229@gmail.com>
2019-11-11 11:06:06 +0100

    stach changes

--- WATTBANK_S_SERIES.X/build/default/production/WS2812.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/production/WS2812.d  \
- build/default/production/WS2812.p1:  \
- WS2812.c  \
-WS2812.h 
--- WATTBANK_S_SERIES.X/build/default/production/lcd.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/lcd.d  \
- build/default/production/lcd.p1:  \
- lcd.c  \
-mcc_generated_files/device_config.h  \
-lcd.h  \
-mcc_generated_files/pin_manager.h 
